ffa30d56d44a3a5868751299e62caf1e700b29ab
kellyrmilligan <kelly.milligan@gmail.com>
2018-03-19 11:20:18 -0500

    hapi-17 tests passing

--- src/plugin/__tests__/index-test.js
+++ src/plugin/__tests__/index-test.js
@@ -25,282 +25,260 @@ const options = {
 const HapiReactRedux = require('plugin/index')
 
 describe('hapi react redux plugin', () => {
-  it('can be registered', (done) => {
+  it('can be registered', async (done) => {
     const server = new Hapi.Server()
-    server.connection()
-    server.register(HapiReactRedux, (err) => {
-      expect(server).toBeTruthy()
-      expect(server.hapiReactRedux).toBeTruthy()
-      expect(err).toBeUndefined()
-      done()
-    })
+    await server.register(HapiReactRedux)
+
+    expect(server).toBeTruthy()
+    expect(server.hapiReactRedux).toBeTruthy()
+    done()
+
   })
 
-  it('can set options with the hapiReactRedux method', (done) => {
+  it('can set options with the hapiReactRedux method', async (done) => {
     const server = new Hapi.Server()
-    server.connection()
-    server.register(HapiReactRedux, (err) => {
-      options.layout = layout
-      options.routes = clientRoutes
-      server.hapiReactRedux(options)
-      server.route({
-        method: 'GET',
-        path: '/',
-        handler (request, reply) {
-          return reply.hapiReactReduxRender()
-        }
-      })
-      server.inject({
-        method: 'GET',
-        url: '/?q=test'
-      }, (res) => {
-        expect(res.result).toMatch(/home/)
-        done()
-      })
+    await server.register(HapiReactRedux)
+
+    options.layout = layout
+    options.routes = clientRoutes
+
+    server.hapiReactRedux(options)
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler (request, h) {
+        return h.hapiReactReduxRender()
+      }
     })
+    const res = await server.inject({
+      method: 'GET',
+      url: '/?q=test'
+    })
+
+    expect(res.result).toMatch(/home/)
+    done()
   })
 
-  it('can have a handler call the hapiReactReduxRender method on reply', (done) => {
+  it('can have a handler call the hapiReactReduxRender method on reply', async (done) => {
     const server = new Hapi.Server()
-    server.connection()
-    server.register(HapiReactRedux, (err) => {
-      options.layout = layout
-      options.routes = clientRoutes
-      server.hapiReactRedux(options)
-      server.route({
-        method: 'GET',
-        path: '/',
-        handler (request, reply) {
-          return reply.hapiReactReduxRender()
-        }
-      })
-      server.inject({
-        method: 'GET',
-        url: '/'
-      }, (res) => {
-        expect(res.result).toMatch(/home/)
-        done()
-      })
+    await server.register(HapiReactRedux)
+    options.layout = layout
+    options.routes = clientRoutes
+    server.hapiReactRedux(options)
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler (request, h) {
+        return h.hapiReactReduxRender()
+      }
     })
+    const res = await server.inject({
+      method: 'GET',
+      url: '/'
+    })
+
+    expect(res.result).toMatch(/home/)
+    done()
   })
 
-  it('can build the location object to match react routers location with pathname and search', (done) => {
+  it('can build the location object to match react routers location with pathname and search', async (done) => {
     const server = new Hapi.Server()
-    server.connection()
-    server.register(HapiReactRedux, (err) => {
-      options.layout = layout
-      options.routes = clientRoutes
-      server.hapiReactRedux(options)
-      server.route({
-        method: 'GET',
-        path: '/search',
-        handler (request, reply) {
-          return reply.hapiReactReduxRender()
-        }
-      })
-      server.inject({
-        method: 'GET',
-        url: '/search?q=test'
-      }, (res) => {
-        expect(res.result).toMatch(/q=test/)
-        done()
-      })
+    await server.register(HapiReactRedux)
+    options.layout = layout
+    options.routes = clientRoutes
+    server.hapiReactRedux(options)
+    server.route({
+      method: 'GET',
+      path: '/search',
+      handler (request, reply) {
+        return reply.hapiReactReduxRender()
+      }
     })
+
+    const res = await server.inject({
+      method: 'GET',
+      url: '/search?q=test'
+    })
+
+    expect(res.result).toMatch(/q=test/)
+    done()
   })
 
-  it('can use the server handler instead of calling the method directly', (done) => {
+  it('can use the server handler instead of calling the method directly', async (done) => {
     const server = new Hapi.Server()
-    server.connection()
-    server.register(HapiReactRedux, (err) => {
-      options.layout = layout
-      options.routes = clientRoutes
-      server.hapiReactRedux(options)
-      server.route({
-        method: 'GET',
-        path: '/',
-        handler: { hapiReactReduxHandler: {} }
-      })
-      server.inject({
-        method: 'GET',
-        url: '/'
-      }, (res) => {
-        expect(res.result).toContain('home')
-        done()
-      })
+    server.register(HapiReactRedux)
+    options.layout = layout
+    options.routes = clientRoutes
+    server.hapiReactRedux(options)
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: { hapiReactReduxHandler: {} }
     })
+    const res = await server.inject({
+      method: 'GET',
+      url: '/'
+    })
+    expect(res.result).toContain('home')
+    done()
   })
 
-  it('can collect data from fetch methods on route handlers to have in the rendered output via react-router-fetch', (done) => {
+  it('can collect data from fetch methods on route handlers to have in the rendered output via react-router-fetch', async (done) => {
     const server = new Hapi.Server()
-    server.connection()
-    server.register(HapiReactRedux, (err) => {
-      options.layout = layout
-      options.routes = clientRoutes
-      server.hapiReactRedux(options)
-      server.route({
-        method: 'GET',
-        path: '/',
-        handler: { hapiReactReduxHandler: {} }
-      })
-      server.inject({
-        method: 'GET',
-        url: '/'
-      }, (res) => {
-        expect(res.result).toMatch(/test-todo-redux/)
-        done()
-      })
+    await server.register(HapiReactRedux)
+    options.layout = layout
+    options.routes = clientRoutes
+    server.hapiReactRedux(options)
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: { hapiReactReduxHandler: {} }
     })
+    const res = await server.inject({
+      method: 'GET',
+      url: '/'
+    })
+
+    expect(res.result).toMatch(/test-todo-redux/)
+    done()
   })
 
-  it('can use data sent to the hapiReactReduxRender method on reply', (done) => {
+  it('can use data sent to the hapiReactReduxRender method on h', async (done) => {
     const server = new Hapi.Server()
-    server.connection()
-    server.register(HapiReactRedux, (err) => {
-      options.layout = layout
-      options.routes = clientRoutes
-      server.hapiReactRedux(options)
-      server.route({
-        method: 'GET',
-        path: '/',
-        handler (request, reply) {
-          return reply.hapiReactReduxRender({
-            test: 'the test'
-          })
-        }
-      })
-      server.inject({
-        method: 'GET',
-        url: '/'
-      }, (res) => {
-        expect(res.result).toMatch(/the test/)
-        done()
-      })
+    await server.register(HapiReactRedux)
+    options.layout = layout
+    options.routes = clientRoutes
+    server.hapiReactRedux(options)
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler (request, h) {
+        return h.hapiReactReduxRender({
+          test: 'the test'
+        })
+      }
     })
+    const res = await server.inject({
+      method: 'GET',
+      url: '/'
+    })
+    expect(res.result).toMatch(/the test/)
+    done()
   })
 
-  it('can use data from route prereqs', (done) => {
+  it('can use data from route prereqs', async (done) => {
     const server = new Hapi.Server()
-    server.connection()
-    server.register(HapiReactRedux, (err) => {
-      options.layout = layout
-      options.routes = clientRoutes
-      server.hapiReactRedux(options)
-      server.route({
-        method: 'GET',
-        path: '/',
-        config: {
-          pre: [
-            { method: (request, reply) => { return reply('preTest') }, assign: 'preTest' }
-          ]
-        },
-        handler (request, reply) {
-          return reply.hapiReactReduxRender()
-        }
-      })
-      server.inject({
-        method: 'GET',
-        url: '/'
-      }, (res) => {
-        expect(res.result).toMatch(/preTest/)
-        done()
-      })
+    server.register(HapiReactRedux)
+    options.layout = layout
+    options.routes = clientRoutes
+    server.hapiReactRedux(options)
+    server.route({
+      method: 'GET',
+      path: '/',
+      config: {
+        pre: [
+          {
+            method: (request, h) => {
+              return 'preTest'
+            },
+            assign: 'preTest'
+          }
+        ]
+      },
+      handler (request, h) {
+        return h.hapiReactReduxRender()
+      }
     })
+    const res = await server.inject({
+      method: 'GET',
+      url: '/'
+    })
+    expect(res.result).toMatch(/preTest/)
+    done()
   })
 
-  it('can use data from config', (done) => {
+  it('can use data from config', async (done) => {
     const server = new Hapi.Server()
-    server.connection()
-    server.register(HapiReactRedux, (err) => {
-      options.layout = layout
-      options.routes = clientRoutes
-      server.hapiReactRedux(options)
-      server.route({
-        method: 'GET',
-        path: '/',
-        handler (request, reply) {
-          return reply.hapiReactReduxRender()
-        }
-      })
-      server.inject({
-        method: 'GET',
-        url: '/'
-      }, (res) => {
-        expect(res.result).toMatch(/1234/)
-        done()
-      })
+    await server.register(HapiReactRedux)
+    options.layout = layout
+    options.routes = clientRoutes
+    server.hapiReactRedux(options)
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler (request, h) {
+        return h.hapiReactReduxRender()
+      }
     })
+    const res = await server.inject({
+      method: 'GET',
+      url: '/'
+    })
+    expect(res.result).toMatch(/1234/)
+    done()
   })
 
-  it('will redirect if RR has a redirect route in it', (done) => {
+  it('will redirect if RR has a redirect route in it', async (done) => {
     const server = new Hapi.Server()
-    server.connection()
-    server.register(HapiReactRedux, (err) => {
-      options.layout = layout
-      options.routes = redirectClientRoutes
-      server.hapiReactRedux(options)
-      server.route({
-        method: 'GET',
-        path: '/{path*}',
-        handler (request, reply) {
-          return reply.hapiReactReduxRender()
-        }
-      })
-      server.inject({
-        method: 'GET',
-        url: '/redirectroute'
-      }, (res) => {
-        expect(res.statusCode).toBe(301)
-        done()
-      })
+    await server.register(HapiReactRedux)
+    options.layout = layout
+    options.routes = redirectClientRoutes
+    server.hapiReactRedux(options)
+    server.route({
+      method: 'GET',
+      path: '/{path*}',
+      handler (request, h) {
+        return h.hapiReactReduxRender()
+      }
     })
+    const res = await server.inject({
+      method: 'GET',
+      url: '/redirectroute'
+    })
+    expect(res.statusCode).toBe(301)
+    done()
   })
 
-  it('will throw error if layout/components are not valid', (done) => {
+  it('will throw error if layout/components are not valid', async (done) => {
     const server = new Hapi.Server()
-    server.connection()
-    server.register(HapiReactRedux, (err) => {
-      options.layout = badLayout
-      options.routes = clientRoutes
-      server.hapiReactRedux(options)
-      server.route({
-        method: 'GET',
-        path: '/',
-        handler (request, reply) {
-          return reply.hapiReactReduxRender()
-        }
-      })
-      server.inject({
-        method: 'GET',
-        url: '/'
-      }, (res) => {
-        expect(res.statusCode).toBe(500)
-        done()
-      })
+    await server.register(HapiReactRedux)
+    options.layout = badLayout
+    options.routes = clientRoutes
+    server.hapiReactRedux(options)
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler (request, h) {
+        return h.hapiReactReduxRender()
+      }
     })
+    const res = await server.inject({
+      method: 'GET',
+      url: '/'
+    })
+    expect(res.statusCode).toBe(500)
+    done()
   })
 
-  it('will 404 if not found', (done) => {
+  it('will 404 if not found', async (done) => {
     const server = new Hapi.Server()
-    server.connection()
-    server.register(HapiReactRedux, (err) => {
-      options.layout = layout
-      options.routes = clientRoutes
-      server.hapiReactRedux(options)
-      server.route({
-        method: 'GET',
-        path: '/{path*}',
-        handler (request, reply) {
-          return reply.hapiReactReduxRender()
-        }
-      })
-      server.inject({
+    server.register(HapiReactRedux)
+    options.layout = layout
+    options.routes = clientRoutes
+    server.hapiReactRedux(options)
+    server.route({
+      method: 'GET',
+      path: '/{path*}',
+      handler (request, reply) {
+        return reply.hapiReactReduxRender()
+      }
+    })
+      const res = await server.inject({
         method: 'GET',
         url: '/notfound'
-      }, (res) => {
-        expect(res.result).toMatch(/Not found/)
-        expect(res.statusCode).toBe(404)
-        done()
       })
-    })
+      expect(res.result).toMatch(/Not found/)
+      expect(res.statusCode).toBe(404)
+      done()
   })
 })
