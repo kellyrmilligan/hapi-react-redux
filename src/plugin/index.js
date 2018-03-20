7bbd890f6746a5d11bcf3e8841fd41a2f32cfdf1
Kelly <kelly.milligan@gmail.com>
2018-03-20 08:18:44 -0500

    Feature/upgrade hapi 17 (#34)
    * updates
    
    * 2.1.0
    
    * hapi-17 tests passing
    
    * no more yarn
    
    * fix linting
    
    * ignore fixtures in linting
    
    * Update README.md

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -9,7 +9,7 @@ import Boom from 'boom'
 import { Provider } from 'react-redux'
 import queryString from 'query-string'
 
-function hapiReactReduxPlugin (server, options, next) {
+async function hapiReactReduxPlugin (server, options, next) {
   server.decorate('server', 'hapiReactRedux', function (options) {
     Hoek.assert(options, 'Missing options')
     Hoek.assert(options.configureStore, 'Missing configure store option')
@@ -20,7 +20,7 @@ function hapiReactReduxPlugin (server, options, next) {
     this.realm.plugins.hapiReactRedux.settings = options
   })
 
-  server.decorate('reply', 'hapiReactReduxRender', function (context) {
+  server.decorate('toolkit', 'hapiReactReduxRender', async function hapiReactReduxRender (context) {
     const realm = this.realm.plugins.hapiReactRedux
     Hoek.assert(realm.settings, 'Cannot render app without settings')
 
@@ -35,47 +35,52 @@ function hapiReactReduxPlugin (server, options, next) {
     const auth = this.request.auth
     const store = configureStore({ auth, pre, config, serverContext: context }) // context is data from the route hander when calling the reply method
 
-    reactRouterFetch(routes, { pathname: decodeURI(this.request.path), search: queryString.stringify(this.request.query) }, { dispatch: store.dispatch, getState: store.getState })
-      .then((results) => {
-        const context = {}
-        let rootHtml = null
-        let layout = null
-        try {
-          rootHtml = renderToString(
-            <Provider store={store}>
-              <StaticRouter location={this.request.path} context={context}>
-                {renderRoutes(routes)}
-              </StaticRouter>
-            </Provider>
-          )
-        } catch (err) {
-          return this.response(Boom.badImplementation(`There was an error rendering the route - ${this.request.raw.req.url}`, err))
-        }
-        try {
-          layout = renderToString(
-            <Layout assets={assets} config={config} content={rootHtml} state={serialize(store.getState(), { isJSON: true })} />
-          )
-        } catch (err) {
-          return this.response(Boom.badImplementation(`There was an error rendering the layout while rendring the route - ${this.request.raw.req.url}`, err))
-        }
-        // this means a redirect component happened somewhere on this  path
-        if (context.url) {
-          return this.redirect(context.url).code(context.statusCode || 301)
-        }
-        this.response(`<!doctype html>\n${layout}`).code(context.statusCode || 200)
+    try {
+      await reactRouterFetch(routes, {
+        pathname: decodeURI(this.request.path),
+        search: queryString.stringify(this.request.query)
+      }, {
+        dispatch: store.dispatch,
+        getState: store.getState
       })
-      .catch((err) => {
-        this.response(Boom.wrap(err))
-      })
-  })
 
-  server.handler('hapiReactReduxHandler', function (route, options) {
-    return function (request, reply) {
-      reply.hapiReactReduxRender()
+      const context = {}
+      let rootHtml = null
+      let layout = null
+      try {
+        rootHtml = renderToString(
+          <Provider store={store}>
+            <StaticRouter location={this.request.path} context={context}>
+              {renderRoutes(routes)}
+            </StaticRouter>
+          </Provider>
+        )
+      } catch (err) {
+        throw Boom.badImplementation(`There was an error rendering the route - ${this.request.raw.req.url}`, err)
+      }
+
+      try {
+        layout = renderToString(
+          <Layout assets={assets} config={config} content={rootHtml} state={serialize(store.getState(), { isJSON: true })} />
+        )
+      } catch (err) {
+        throw Boom.badImplementation(`There was an error rendering the layout while rendring the route - ${this.request.raw.req.url}`, err)
+      }
+      // this means a redirect component happened somewhere on this  path
+      if (context.url) {
+        return this.redirect(context.url).code(context.statusCode || 301)
+      }
+      return this.response(`<!doctype html>\n${layout}`).code(context.statusCode || 200)
+    } catch (err) {
+      throw Boom.boomify(err)
     }
   })
 
-  next()
+  server.decorate('handler', 'hapiReactReduxHandler', function (route, options) {
+    return function (request, h) {
+      return h.hapiReactReduxRender()
+    }
+  })
 }
 
 hapiReactReduxPlugin.attributes = {
@@ -83,5 +88,6 @@ hapiReactReduxPlugin.attributes = {
 }
 
 module.exports = {
-  register: hapiReactReduxPlugin
+  register: hapiReactReduxPlugin,
+  name: 'hapi-react-redux'
 }
